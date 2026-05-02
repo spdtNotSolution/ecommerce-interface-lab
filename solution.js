c5ab33dacad694f5b749b0752cd8cfbdb844b733
VarelaChristianJr.A. <christianvarelajr@gmail.com>
2026-05-02 10:10:02 +0800

    implemet frontend fetch logic for product

--- solution.js
+++ solution.js
@@ -1,4 +1,4 @@
- // Problem 5: The Private Inventory
+// Problem 5: The Private Inventory
 class Item {
     #discount = 0.1; // private property
 
@@ -12,3 +12,36 @@ class Item {
         return this.price - (this.price * this.#discount);
     }
 }
+
+
+const API_BASE_URL = 'http://localhost:8080/api/v1';
+
+async function fetchProducts() {
+    try {
+        const response = await fetch(`${API_BASE_URL}/products`);
+        
+        if (!response.ok) {
+            if (response.status === 404) {
+                throw new Error(`Products not found - 404`);
+            } else if (response.status === 500) {
+                throw new Error(`Server error - 500`);
+            }
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        
+        const products = await response.json();
+        console.log(`Products fetched:, products`);
+        return products;
+        
+    } catch (error) {
+        console.error(`Fetch API Error:`, error.message);
+        const main = document.querySelector(`main`);
+        if (main) {
+            main.innerHTML = `<h2>Error: ${error.message}</h2>`;
+        }
+        return [];
+    }
+}
+
+
+fetchProducts();
